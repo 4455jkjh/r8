a3c60e379a6bc0f909c7287410d0c06e2fadc779
Søren Gjesse <sgjesse@google.com>
2023-11-15 09:27:06 +0100

    Version 8.1.73
    Bug: b/310939676
    Change-Id: Ia75b53ba31670e60b91950dcd371e77a30004d45

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.1.72";
+  public static final String LABEL = "8.1.73";
 
   private Version() {
   }
