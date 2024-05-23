c6912214093ba0758d935e94d2b5d42eeaee733e
Søren Gjesse <sgjesse@google.com>
2024-05-23 09:34:22 +0200

    Version 8.2.57
    Fix git merge issue.
    
    Change-Id: I768777987f447173d92e0bf90bc8d244b0c9cbe9

--- src/main/java/com/android/tools/r8/ir/conversion/passes/NaturalIntLoopRemover.java
+++ src/main/java/com/android/tools/r8/ir/conversion/passes/NaturalIntLoopRemover.java
@@ -18,6 +18,7 @@ import com.android.tools.r8.ir.code.Value;
 import com.android.tools.r8.ir.conversion.passes.result.CodeRewriterResult;
 import com.android.tools.r8.ir.optimize.AffectedValues;
 import com.android.tools.r8.utils.WorkList;
+import com.google.common.collect.Sets;
 import java.util.Set;
 
 /**
