ab5db111a514eae525deae65f3b11d5c5b356a4c
Ian Zerny <zerny@google.com>
2024-05-30 09:40:15 +0200

    Regression test for array clone in default or static interface methods
    Edit: this CL also includes the workaround to avoid the issue.
    
    Bug: b/342802978
    Change-Id: I25e4b4bc44e0286dbd3e2f91102726527af9c3c1

--- src/main/java/com/android/tools/r8/ir/desugar/NonEmptyCfInstructionDesugaringCollection.java
+++ src/main/java/com/android/tools/r8/ir/desugar/NonEmptyCfInstructionDesugaringCollection.java
@@ -124,6 +124,8 @@ public class NonEmptyCfInstructionDesugaringCollection extends CfInstructionDesu
                 lambdaDesugaring, stringConcatDesugaring, recordRewriter));
     if (interfaceMethodRewriter != null) {
       desugarings.add(interfaceMethodRewriter);
+    } else if (appView.options().canHaveArtArrayCloneFromInterfaceMethodBug()) {
+      desugarings.add(new OutlineArrayCloneFromInterfaceMethodDesugaring(appView));
     }
     desugaredLibraryAPIConverter =
         appView.typeRewriter.isRewriting()
@@ -178,6 +180,10 @@ public class NonEmptyCfInstructionDesugaringCollection extends CfInstructionDesu
     NonEmptyCfInstructionDesugaringCollection desugaringCollection =
         new NonEmptyCfInstructionDesugaringCollection(appView, noAndroidApiLevelCompute());
     desugaringCollection.desugarings.add(new InvokeSpecialToSelfDesugaring(appView));
+    if (appView.options().canHaveArtArrayCloneFromInterfaceMethodBug()) {
+      desugaringCollection.desugarings.add(
+          new OutlineArrayCloneFromInterfaceMethodDesugaring(appView));
+    }
     desugaringCollection.yieldingDesugarings.add(
         new UnrepresentableInDexInstructionRemover(appView));
     return desugaringCollection;
