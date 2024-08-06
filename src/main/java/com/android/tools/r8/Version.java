6ad8957e838f3a3ccf8c08a0e7a93e1420ad5b76
Rico Wind <ricow@google.com>
2024-08-06 13:40:28 +0200

    Version 8.7.4-dev
    Change-Id: I9bf8fc4826c2531f62d992ec7f95f657a01d72e4

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.7.3-dev";
+  public static final String LABEL = "8.7.4-dev";
 
   private Version() {
   }
