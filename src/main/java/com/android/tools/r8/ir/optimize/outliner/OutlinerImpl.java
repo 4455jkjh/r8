03a0861b68f732ea29f828562f8f7d86a4a85d79
Søren Gjesse <sgjesse@google.com>
2024-08-15 09:13:49 +0200

    Fix invalid rewriting in outliner
    When splitting blocks after inserting an invoke to an outline the block
    after the split was not fully processed leaving some of the outlined
    instructions.
    
    Bug: b/353279141
    Change-Id: Ib74460c93b1bf46ce2d1cad602b804613b9b1205

--- src/main/java/com/android/tools/r8/ir/optimize/outliner/OutlinerImpl.java
+++ src/main/java/com/android/tools/r8/ir/optimize/outliner/OutlinerImpl.java
@@ -35,6 +35,7 @@ import com.android.tools.r8.ir.code.Add;
 import com.android.tools.r8.ir.code.Assume;
 import com.android.tools.r8.ir.code.BasicBlock;
 import com.android.tools.r8.ir.code.BasicBlock.ThrowingInfo;
+import com.android.tools.r8.ir.code.BasicBlockIterator;
 import com.android.tools.r8.ir.code.Binop;
 import com.android.tools.r8.ir.code.CatchHandlers;
 import com.android.tools.r8.ir.code.Div;
@@ -90,7 +91,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
@@ -1659,24 +1659,26 @@ public class OutlinerImpl extends Outliner {
     if (!toRemove.isEmpty()) {
       assert !invokesToOutlineMethods.isEmpty();
       // Scan over the entire code to remove outline instructions.
-      ListIterator<BasicBlock> blocksIterator = code.listIterator();
+      BasicBlockIterator blocksIterator = code.listIterator();
       while (blocksIterator.hasNext()) {
         BasicBlock block = blocksIterator.next();
         InstructionListIterator instructionListIterator = block.listIterator(code);
         instructionListIterator.forEachRemaining(
             instruction -> {
-              if (toRemove.contains(instruction)) {
+              if (toRemove.remove(instruction)) {
                 instructionListIterator.removeInstructionIgnoreOutValue();
               } else if (invokesToOutlineMethods.contains(instruction)
                   && block.hasCatchHandlers()) {
                 // If the inserted invoke is inserted in a block with handlers, split the block
                 // after the inserted invoke.
-                instructionListIterator.split(code, blocksIterator);
+                instructionListIterator.splitCopyCatchHandlers(
+                    code, blocksIterator, appView.options(), ignored -> block);
               }
             });
       }
       code.removeRedundantBlocks();
     }
+    assert toRemove.isEmpty();
     code.removeRedundantBlocks();
     assert code.isConsistentSSA(appView);
   }
