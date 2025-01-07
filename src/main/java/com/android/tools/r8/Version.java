b6b2b638738ed9914c3cbc7103ae1a236354c1b4
Christoffer Adamsen <christofferqa@google.com>
2025-01-07 10:45:32 +0100

    Version 8.9.19-dev
    Change-Id: I5056e127f91d95adbf37c1b8ed5b422d9b998e8b

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.9.18-dev";
+  public static final String LABEL = "8.9.19-dev";
 
   private Version() {
   }
