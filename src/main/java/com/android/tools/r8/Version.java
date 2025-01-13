fdf74442918c2100d22c5bbb3c185044a58144cf
Søren Gjesse <sgjesse@google.com>
2025-01-13 09:58:25 +0100

    Version 8.7.28
    R=christofferqa@google.com
    
    Bug: b/387258081
    Change-Id: I72cc81fbfb033dad6a78d61c3d9b392c7c326a7f

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.7.27";
+  public static final String LABEL = "8.7.28";
 
   private Version() {
   }
