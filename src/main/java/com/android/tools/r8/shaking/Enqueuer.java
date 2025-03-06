5cc2d659e1fd08cecbc8f7cfcbfc404d0bc95fa6
Christoffer Adamsen <christofferqa@google.com>
2025-03-06 12:12:03 +0100

    Fix BootstrapMethodError from invalid class merging
    Bug: b/397737234
    Change-Id: Icacf51f06017c50b3448610e8493a225c225c40d

--- src/main/java/com/android/tools/r8/shaking/Enqueuer.java
+++ src/main/java/com/android/tools/r8/shaking/Enqueuer.java
@@ -1282,7 +1282,9 @@ public class Enqueuer {
         traceInvokeDirectFromLambda(method, context, registry);
         break;
       case INVOKE_CONSTRUCTOR:
+        assert appView.dexItemFactory().isConstructor(method);
         traceNewInstanceFromLambda(method.holder, context);
+        traceInvokeDirectFromLambda(method, context, registry);
         break;
       default:
         throw new Unreachable();
