9fdb7317b319ede15492aab3bc78326556d01e5c
Rico Wind <ricow@google.com>
2025-03-10 08:12:41 +0100

    Version 8.7.38
    Bug: b/401137827
    Change-Id: Ia5adc851e21055d2dee45aedcfdbe8cf61312e05

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.7.37";
+  public static final String LABEL = "8.7.38";
 
   private Version() {
   }
