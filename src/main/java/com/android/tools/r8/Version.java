15a3b0ab8770e5bc01f6d80fbd2875f758ce6df3
Clément Béra <clementbera@google.com>
2025-03-12 10:25:36 +0100

    Version 8.11.2-dev
    Change-Id: I4f8d004706331d072443c9d50551e90e2e874558

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.11.1-dev";
+  public static final String LABEL = "8.11.2-dev";
 
   private Version() {
   }
