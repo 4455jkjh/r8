ee2c4ec5788759681bf8fa85a23a2ecc84132103
Rico Wind <ricow@google.com>
2025-03-21 13:29:27 +0100

    Fix index for feature split dex code in legacy resource shrinking
    This would always be zero, causing us to only trace the dex code for the
    last feature split entry in legacy mode.
    
    Add regression test for both optimized(already passing) and
    legacy(failing without the fix here).
    
    Bug: b/404745556
    Change-Id: Ia7f0e149a1c0d0c556f1c8bee7c3968fc0649cce

--- src/main/java/com/android/tools/r8/R8.java
+++ src/main/java/com/android/tools/r8/R8.java
@@ -875,7 +875,7 @@ public class R8 {
                 wrapConsumerStoreBytesInList(
                     dexFileContent,
                     (DexIndexedConsumer) featureSplit.getProgramConsumer(),
-                    "feature" + featureIndex));
+                    "feature" + featureIndex++));
           }
         }
       }
