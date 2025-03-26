c2cd37af9db6e5b4cdad9784e7ea5044fbdb5d0e
Søren Gjesse <sgjesse@google.com>
2025-03-26 13:47:26 +0100

    Version 8.11.6-dev
    Change-Id: If15de4d525935b8eabe6f7d3e0dfeea5d576475e

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.11.5-dev";
+  public static final String LABEL = "8.11.6-dev";
 
   private Version() {
   }
