5241bf06fcf8801beaf852e2f240a65a3ed89948
Christoffer Adamsen <christofferqa@google.com>
2025-08-05 11:00:31 +0200

    Version 8.11.21
    Bug: b/434861604
    Change-Id: I6609440836530950f72fb979b007546b1448cce2

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.11.20";
+  public static final String LABEL = "8.11.21";
 
   private Version() {
   }
