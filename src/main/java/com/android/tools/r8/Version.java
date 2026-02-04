b358c3258fd2cdb3ae1e3c5aafd24883e37f2a48
Christoffer Adamsen <christofferqa@google.com>
2026-02-04 15:01:24 +0100

    Version 9.1.28-dev
    Change-Id: I8a62b67df57fa5970a01710befb32f9ec8c95ff2

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -15,7 +15,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "9.1.27-dev";
+  public static final String LABEL = "9.1.28-dev";
 
   private Version() {
   }
