64db0337193bc09523c36b937fa8f5a5df234931
Jonathan Starup <jonathanlist@google.com>
2026-02-27 14:31:51 +0000

    Polish upload_to_x20.py.
    Change-Id: I105b6e58e7449324de63ea9a2149ffc382ec6133

--- tools/upload_to_x20.py
+++ tools/upload_to_x20.py
@@ -3,32 +3,29 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-# Script for uploading to x20 as a dependency in the same way we use cloud
-# storage.
+# Script for uploading to x20 as a dependency in the same way we use cloud storage.
 
 import optparse
 import os
 import shutil
-import stat
 import subprocess
 import sys
-import tarfile
 import utils
 
-GMSCORE_DEPS = '/google/data/rw/teams/r8/deps'
+GMS_CORE_DEPS = '/google/data/rw/teams/r8/deps'
 
 
 def parse_options():
     return optparse.OptionParser().parse_args()
 
 
-def uploadFile(filename, dest):
+def upload_file(filename, dest):
     print('Uploading to %s' % dest)
     shutil.copyfile(filename, dest)
     subprocess.check_call(['chmod', '664', dest])
 
 
-def Main():
+def main():
     (options, args) = parse_options()
     assert len(args) == 1
     name = args[0]
@@ -40,13 +37,14 @@ def Main():
         return 1
     filename = utils.create_archive(name)
     sha1 = utils.get_sha1(filename)
-    dest = os.path.join(GMSCORE_DEPS, sha1)
-    uploadFile(filename, dest)
+    dest = os.path.join(GMS_CORE_DEPS, sha1)
+    upload_file(filename, dest)
     sha1_file = '%s.sha1' % filename
     with open(sha1_file, 'w') as output:
         output.write(sha1)
     print('Sha (%s) written to: %s' % (sha1, sha1_file))
+    return 0
 
 
 if __name__ == '__main__':
-    sys.exit(Main())
+    sys.exit(main())
