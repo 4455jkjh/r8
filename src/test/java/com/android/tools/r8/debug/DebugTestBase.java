06d3b3603a82f167f68875de89ccdf715a560a3b
Johan Bay <jobay@google.com>
2026-04-23 10:37:28 +0200

    Hoist @Retryable to DebugTestBase
    Change-Id: Ia8ea7fe199262add15da59099a9263e77bfcfeb9

--- src/test/java/com/android/tools/r8/debug/DebugTestBase.java
+++ src/test/java/com/android/tools/r8/debug/DebugTestBase.java
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.debug;
 
+import com.android.tools.r8.Retryable;
 import com.android.tools.r8.TestBase;
 import com.android.tools.r8.TestRuntime;
 import com.android.tools.r8.ToolHelper;
@@ -79,9 +80,10 @@ import org.junit.rules.TestName;
 /**
  * Base class for debugging tests.
  *
- * The protocol messages are described here:
+ * <p>The protocol messages are described here:
  * https://docs.oracle.com/javase/8/docs/platform/jpda/jdwp/jdwp-protocol.html
  */
+@Retryable
 public abstract class DebugTestBase extends TestBase {
 
   // Set to true to enable verbose logs
