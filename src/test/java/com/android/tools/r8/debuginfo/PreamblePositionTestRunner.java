1c7e600b92244271fa697e652d6fb987baf03518
Søren Gjesse <sgjesse@google.com>
2026-05-05 12:54:53 +0200

    Update more test expectations for ART 17
    Bug: b/505730602
    Change-Id: If3f73cdd9765cba6f5eaa3fdd496d8c407c86f04

--- src/test/java/com/android/tools/r8/debuginfo/PreamblePositionTestRunner.java
+++ src/test/java/com/android/tools/r8/debuginfo/PreamblePositionTestRunner.java
@@ -4,76 +4,78 @@
 
 package com.android.tools.r8.debuginfo;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static com.android.tools.r8.CompilationMode.RELEASE;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 
-import com.android.tools.r8.CompilationMode;
-import com.android.tools.r8.D8Command;
-import com.android.tools.r8.OutputMode;
 import com.android.tools.r8.TestBase;
-import com.android.tools.r8.ToolHelper;
-import com.android.tools.r8.ToolHelper.ArtCommandBuilder;
-import com.android.tools.r8.ToolHelper.ProcessResult;
+import com.android.tools.r8.TestCompileResult;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.ToolHelper.DexVm.Version;
+import com.android.tools.r8.utils.internal.BooleanUtils;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class PreamblePositionTestRunner extends TestBase {
 
   private static final String TEST_CLASS = "PreamblePositionTestSource";
   private static final String TEST_PACKAGE = "com.android.tools.r8.debuginfo";
 
-  @Test
-  public void testBothBranchesDefaultConditionals() throws Exception {
-    testBothBranches(false);
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameter(1)
+  public boolean invertConditionals;
+
+  @Parameters(name = "{0}, invertConditionals: {1}")
+  public static List<Object[]> data() {
+    return buildParameters(
+        getTestParameters().withDexRuntimes().withAllApiLevels().build(), BooleanUtils.values());
   }
 
   @Test
-  public void testBothBranchesInvertConditionals() throws Exception {
-    testBothBranches(true);
-  }
-
-  private void testBothBranches(boolean invertConditionals) throws Exception {
+  public void testBothBranches() throws Exception {
     Path testClassPath = temp.newFolder().toPath().resolve(TEST_CLASS + ".class");
-    Path outputDexPath = temp.newFolder().toPath();
 
     Files.write(testClassPath, PreamblePositionTestSourceDump.dump());
 
-    ToolHelper.runD8(
-        D8Command.builder()
+    TestCompileResult<?, ?> result =
+        testForD8(parameters)
             .addProgramFiles(testClassPath)
-            .setOutput(outputDexPath, OutputMode.DexIndexed)
-            .setMode(CompilationMode.RELEASE),
-        options -> {
-          options.testing.invertConditionals = invertConditionals;
-        });
-
+            .setMode(RELEASE)
+            .addOptionsModification(
+                options -> options.testing.invertConditionals = invertConditionals)
+            .compile();
     String fileName = TEST_CLASS + ".java";
 
-    for (boolean testTrueBranch : new boolean[] {false, true}) {
-      ArtCommandBuilder artCommandBuilder = new ArtCommandBuilder();
-      artCommandBuilder.appendClasspath(outputDexPath.resolve("classes.dex").toString());
-      artCommandBuilder.setMainClass(TEST_PACKAGE + "." + TEST_CLASS);
-      if (!testTrueBranch) {
-        artCommandBuilder.appendProgramArgument("1");
-      }
+    result
+        .run(parameters.getRuntime(), TEST_PACKAGE + "." + TEST_CLASS)
+        .assertFailureWithErrorThatMatches(containsString("<true-branch-exception>"))
+        .assertFailure()
+        // Must have either explicit line = 0 or no line info at all unless ART 17 or newer.
+        .assertFailureWithErrorThatMatches(
+            anyOf(
+                containsString(fileName + ":0"),
+                allOf(
+                    containsString("at " + TEST_PACKAGE + "." + TEST_CLASS + ".main"),
+                    (parameters.getDexRuntimeVersion().isNewerThanOrEqual(Version.V17_0_0)
+                        ? containsString(fileName + ":")
+                        : (not(containsString(fileName + ":")))))));
 
-      ProcessResult result = ToolHelper.runArtRaw(artCommandBuilder);
-
-      assertNotEquals(result.exitCode, 0);
-      if (testTrueBranch) {
-        assertTrue(result.stderr.contains("<true-branch-exception>"));
-        // Must have either explicit line = 0 or no line info at all.
-        assertTrue(
-            result.stderr.contains(fileName + ":0")
-                || (result.stderr.contains("at " + TEST_PACKAGE + "." + TEST_CLASS + ".main")
-                    && !result.stderr.contains(fileName + ":")));
-      } else {
-        assertTrue(result.stderr.contains("<false-branch-exception>"));
-        assertTrue(
-            result.stderr.contains(
+    result
+        .run(parameters.getRuntime(), TEST_PACKAGE + "." + TEST_CLASS, "1")
+        .assertFailureWithErrorThatMatches(containsString("<false-branch-exception>"))
+        .assertFailureWithErrorThatMatches(
+            containsString(
                 fileName + ":" + PreamblePositionTestSourceDump.FALSE_BRANCH_LINE_NUMBER));
-      }
-    }
   }
 }
