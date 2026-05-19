a6a0c0489f86be8d2ce4d1e521e0148935bad30c
Johan Bay <jobay@google.com>
2026-05-19 11:20:02 +0200

    Fix race condition in AnnotationRemover.Builder annotation retention
    During Enqueuer tracing, multiple threads concurrently register matched
    annotations for retention. AnnotationRemover.Builder.annotationsToRetain
    was a non-thread-safe IdentityHashSet, leading to non-deterministic
    lost updates (race on write) and corrupted resizes.
    
    Change-Id: Icc8b7ea161cb316a873a813735381c51e76f2783

--- src/main/java/com/android/tools/r8/shaking/AnnotationRemover.java
+++ src/main/java/com/android/tools/r8/shaking/AnnotationRemover.java
@@ -32,7 +32,9 @@ import com.android.tools.r8.shaking.Enqueuer.Mode;
 import com.android.tools.r8.utils.ArrayUtils;
 import com.android.tools.r8.utils.InternalOptions;
 import com.android.tools.r8.utils.ThreadUtils;
+import com.android.tools.r8.utils.SetUtils;
 import com.google.common.collect.Sets;
+import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -477,7 +479,8 @@ public class AnnotationRemover {
      * The set of annotations that were matched by a conditional if rule. These are needed for the
      * interpretation of if rules in the second round of tree shaking.
      */
-    private final Set<DexAnnotation> annotationsToRetain = Sets.newIdentityHashSet();
+    private final Set<DexAnnotation> annotationsToRetain =
+        Collections.synchronizedSet(Sets.newIdentityHashSet());
 
     private final Mode mode;
 
@@ -494,7 +497,7 @@ public class AnnotationRemover {
     }
 
     public AnnotationRemover build(AppView<AppInfoWithLiveness> appView) {
-      return new AnnotationRemover(appView, annotationsToRetain, mode);
+      return new AnnotationRemover(appView, SetUtils.newIdentityHashSet(annotationsToRetain), mode);
     }
   }
 }
