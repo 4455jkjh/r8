eaaf7b14a168d5d1fb2e9ba071d54b967de7d67b
Jonathan Starup <jonathanlist@google.com>
2026-06-17 11:44:55 +0000

    Move classmerging tests into their own module.
    Bug: b/518716827
    Change-Id: Idffa1e080d1314b09ce726411254c7ff9a940d48

--- src/test/java8/classmerging/com/android/tools/r8/classmerging/horizontal/ReserveInterfaceMethodSignaturesInFixupTest.java
+++ src/test/java8/classmerging/com/android/tools/r8/classmerging/horizontal/ReserveInterfaceMethodSignaturesInFixupTest.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-package com.android.tools.r8.horizontalclassmerging;
+package com.android.tools.r8.classmerging.horizontal;
 
 import com.android.tools.r8.NeverInline;
 import com.android.tools.r8.NoHorizontalClassMerging;
