0bfdd9dab62475c06f2e59bf24ee5d1ea5182fa4
Christoffer Adamsen <christofferqa@google.com>
2026-06-25 13:47:27 +0200

    Add an ApkAnalyzer in a new tools module for extracting info from apks
    Change-Id: I07c1547b25925b2f99a878ad169bfacbf13c88fc

---
+++ src/tools/java/com/android/tools/r8/tools/apkanalyzer/ApkAnalyzerResult.java
@@ -0,0 +1,73 @@
+// Copyright (c) 2026, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.tools.apkanalyzer;
+
+import com.android.tools.r8.dex.Marker;
+import java.util.List;
+
+class ApkAnalyzerResult {
+
+  // Number of compressed dex files.
+  final int dexCompressedCount;
+
+  // Dex size stats.
+  final MinMaxTotalStats dexSize;
+
+  // Constant pool stats.
+  final MinMaxTotalStats fields;
+  final MinMaxTotalStats methods;
+  final MinMaxTotalStats types;
+
+  // Debug info stats.
+  final int debugInfoNone;
+  final int debugInfoEmbeddedPc;
+  final int debugInfoEventBased;
+
+  // Desugared library info.
+  final DesugaredLibraryInfo desugaredLibraryInfo;
+
+  // Marker stats.
+  final List<Marker> markers;
+
+  // Source file stats.
+  final String mostOccurringSourceFile;
+  final int mostOccurringSourceFileCount;
+
+  // Annotation stats.
+  final int runtimeInvisibleAnnotations;
+
+  // Class depth stats.
+  final int[] classDepthCounts;
+
+  ApkAnalyzerResult(
+      int dexCountCompressed,
+      MinMaxTotalStats dexSize,
+      MinMaxTotalStats types,
+      MinMaxTotalStats fields,
+      MinMaxTotalStats methods,
+      List<Marker> markers,
+      DesugaredLibraryInfo desugaredLibInfo,
+      int debugInfoNone,
+      int debugInfoEmbeddedPc,
+      int debugInfoEventBased,
+      String mostOccurringSourceFile,
+      int mostOccurringSourceFileCount,
+      int runtimeInvisibleAnnotations,
+      int[] classDepthCounts) {
+    this.dexCompressedCount = dexCountCompressed;
+    this.dexSize = dexSize;
+    this.types = types;
+    this.fields = fields;
+    this.methods = methods;
+    this.markers = markers;
+    this.desugaredLibraryInfo = desugaredLibInfo;
+    this.debugInfoNone = debugInfoNone;
+    this.debugInfoEmbeddedPc = debugInfoEmbeddedPc;
+    this.debugInfoEventBased = debugInfoEventBased;
+    this.mostOccurringSourceFile = mostOccurringSourceFile;
+    this.mostOccurringSourceFileCount = mostOccurringSourceFileCount;
+    this.runtimeInvisibleAnnotations = runtimeInvisibleAnnotations;
+    this.classDepthCounts = classDepthCounts;
+  }
+}
