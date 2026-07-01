1ac15c738aa91cad4659924cc490463f1f82bdc7
Jonathan Starup <jonathanlist@google.com>
2026-07-01 09:19:33 +0000

    Use CliParser in GlobalSyntheticsGeneratorCommandParser.
    Bug: b/510714591
    Change-Id: I0d0f2b163e7f36b02e82d109d62a4f814a8a4239

--- src/test/java/com/android/tools/r8/GlobalSyntheticsGeneratorCommandParserTest.java
+++ src/test/java/com/android/tools/r8/GlobalSyntheticsGeneratorCommandParserTest.java
@@ -5,7 +5,10 @@
 package com.android.tools.r8;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
+import com.android.tools.r8.origin.Origin;
 import com.android.tools.r8.utils.internal.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,19 +32,36 @@ public class GlobalSyntheticsGeneratorCommandParserTest extends TestBase {
     assertEquals(
         StringUtils.lines(
             "Usage: globalsyntheticsgenerator [options] where options are:",
-            "",
             "  --min-api <number>      # Minimum Android API level compatibility (default: 1).",
             "  --lib <file|jdk-home>   # Add <file|jdk-home> as a library resource.",
             "  --output <globals-file> # Output result in <globals-file>.",
             "  --classfile             # Generate globals for only classfile to classfile"
-                + " desugaring.",
-            "                          # (By default globals for both classfile and dex desugaring"
-                + " are generated).",
+                + " desugaring. (By default",
+            "                          # globals for both classfile and dex desugaring are"
+                + " generated).",
             "  --verbose-synthetic-names",
             "                          # Enable verbose synthetic names that use the"
                 + " `$$ExternalSynthetic` marker.",
             "  --version               # Print the version of globalsyntheticsgenerator.",
             "  --help                  # Print this message."),
         GlobalSyntheticsGeneratorCommandParser.getUsageMessage());
+  }
+
+  @Test
+  public void testParseDefault() {
+    GlobalSyntheticsGeneratorCommand command =
+        GlobalSyntheticsGeneratorCommand.parse(new String[0], Origin.unknown()).build();
+    assertNotNull(command.getInternalOptions().getGlobalSyntheticsConsumer());
+    assertFalse(command.isPrintHelp());
+    assertFalse(command.isPrintVersion());
+  }
+
+  @Test
+  public void testParseOutput() {
+    GlobalSyntheticsGeneratorCommand command =
+        GlobalSyntheticsGeneratorCommand.parse(
+                new String[] {"--output", "out.jar"}, Origin.unknown())
+            .build();
+    assertNotNull(command.getInternalOptions().getGlobalSyntheticsConsumer());
   }
 }
