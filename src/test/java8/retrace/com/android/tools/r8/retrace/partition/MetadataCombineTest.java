8d7f0a937ea5c75c171d85b93817c969da167ebb
Søren Gjesse <sgjesse@google.com>
2026-08-13 11:59:50 +0200

    Support combining partition mapping files for non -dev versions
    This is a minimal change allowing the mapping header to not have the
    compiler_hash line.
    
    Bug: b/545897331
    
    Change-Id: I9e26aada7f41a9964ef73480c273cfb4692f1c51

--- src/test/java8/retrace/com/android/tools/r8/retrace/partition/MetadataCombineTest.java
+++ src/test/java8/retrace/com/android/tools/r8/retrace/partition/MetadataCombineTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 
 public class MetadataCombineTest extends TestBase {
 
-  private static final List<String> VALID_HEADER =
+  private static final List<String> VALID_HEADER_MAIN =
       Arrays.asList(
           "# compiler: R8",
           "# compiler_version: main",
@@ -27,13 +27,29 @@ public class MetadataCombineTest extends TestBase {
           "# common_typos_disable",
           "# {\"id\":\"com.android.tools.r8.mapping\",\"version\":\"2.2\"}");
 
-  @Test
-  public void testCombineSuccess() {
-    List<String> preamble1 = new java.util.ArrayList<>(VALID_HEADER);
+  private static final List<String> VALID_HEADER_DEV =
+      Arrays.asList(
+          "# compiler: R8",
+          "# compiler_version: 1.2.3-dev",
+          "# min_api: 26",
+          "# compiler_hash: 1234567890abcdef1234567890abcdef",
+          "# common_typos_disable",
+          "# {\"id\":\"com.android.tools.r8.mapping\",\"version\":\"2.2\"}");
+
+  private static final List<String> VALID_HEADER_STABLE =
+      Arrays.asList(
+          "# compiler: R8",
+          "# compiler_version: 1.2.3",
+          "# min_api: 26",
+          "# common_typos_disable",
+          "# {\"id\":\"com.android.tools.r8.mapping\",\"version\":\"2.2\"}");
+
+  public void testCombineSuccess(List<String> valid_header) {
+    List<String> preamble1 = new java.util.ArrayList<>(valid_header);
     preamble1.add("# pg_map_id: id1");
     preamble1.add("# pg_map_hash: SHA-256 hash1");
 
-    List<String> preamble2 = new java.util.ArrayList<>(VALID_HEADER);
+    List<String> preamble2 = new java.util.ArrayList<>(valid_header);
     preamble2.add("# pg_map_id: id2");
     preamble2.add("# pg_map_hash: SHA-256 hash2");
 
@@ -43,11 +59,11 @@ public class MetadataCombineTest extends TestBase {
     MetadataAdditionalInfo combined = info1.combine(info2, "new_id");
 
     List<String> combinedPreamble = new java.util.ArrayList<>(combined.getPreamble());
-    assertEquals(8, combinedPreamble.size());
-    for (int i = 0; i <= 5; i++) {
-      assertEquals(VALID_HEADER.get(i), combinedPreamble.get(i));
+    assertEquals(preamble1.size(), combinedPreamble.size());
+    for (int i = 0; i < valid_header.size(); i++) {
+      assertEquals(valid_header.get(i), combinedPreamble.get(i));
     }
-    assertEquals("# pg_map_id: new_id", combinedPreamble.get(6));
+    assertEquals("# pg_map_id: new_id", combinedPreamble.get(preamble1.size() - 2));
 
     String expectedHash =
         Hashing.sha256()
@@ -56,16 +72,24 @@ public class MetadataCombineTest extends TestBase {
             .putString("hash2", StandardCharsets.UTF_8)
             .hash()
             .toString();
-    assertEquals("# pg_map_hash: SHA-256 " + expectedHash, combinedPreamble.get(7));
+    assertEquals(
+        "# pg_map_hash: SHA-256 " + expectedHash, combinedPreamble.get(preamble1.size() - 1));
+  }
+
+  @Test
+  public void testCombineSuccess() {
+    testCombineSuccess(VALID_HEADER_MAIN);
+    testCombineSuccess(VALID_HEADER_DEV);
+    testCombineSuccess(VALID_HEADER_STABLE);
   }
 
   @Test
   public void testCombineHeaderMismatch() {
-    List<String> preamble1 = new java.util.ArrayList<>(VALID_HEADER);
+    List<String> preamble1 = new java.util.ArrayList<>(VALID_HEADER_DEV);
     preamble1.add("# pg_map_id: id1");
     preamble1.add("# pg_map_hash: SHA-256 hash1");
 
-    List<String> preamble2 = new java.util.ArrayList<>(VALID_HEADER);
+    List<String> preamble2 = new java.util.ArrayList<>(VALID_HEADER_DEV);
     preamble2.set(2, "# min_api: 24"); // Mismatch
     preamble2.add("# pg_map_id: id2");
     preamble2.add("# pg_map_hash: SHA-256 hash2");
@@ -79,7 +103,7 @@ public class MetadataCombineTest extends TestBase {
   @Test
   public void testCombinePreambleTooShort() {
     List<String> preamble1 = Arrays.asList("# compiler: R8");
-    List<String> preamble2 = new java.util.ArrayList<>(VALID_HEADER);
+    List<String> preamble2 = new java.util.ArrayList<>(VALID_HEADER_DEV);
     preamble2.add("# pg_map_id: id2");
     preamble2.add("# pg_map_hash: SHA-256 hash2");
 
