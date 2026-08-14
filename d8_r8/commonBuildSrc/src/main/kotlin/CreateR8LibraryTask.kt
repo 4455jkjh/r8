9d2a04b8f85b195ed803622d6ea621687ce60221
Aurimas Liutikas <aurimas@google.com>
2026-08-14 11:20:34 -0700

    Reapply "Rewrite more swissarmyknife tasks"
    This reverts commit 28fe7f731058174d60edd33f3a9baba1ebe47065.
    
    Reapply "Rewrite :test SwissArmyKnife tasks"
    
    This reverts commit c69135776728d1d8f6c6530f30ba3b708de97cc3.
    
    Reapply "Rewrite :test usages of create_r8lib.py"
    
    This reverts commit 7f8c3083670f1bcfc5973c1d477c43cf2837ff3f.
    
    Change-Id: Ib0f55c724ca8283ce996487aebe7ad1904a0444f

--- d8_r8/commonBuildSrc/src/main/kotlin/CreateR8LibraryTask.kt
+++ d8_r8/commonBuildSrc/src/main/kotlin/CreateR8LibraryTask.kt
@@ -4,12 +4,17 @@
 
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.net.URI.create
 import java.net.URLClassLoader
 import java.nio.charset.Charset
 import java.nio.file.FileSystems.newFileSystem
 import java.nio.file.Files.deleteIfExists
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
 import javax.inject.Inject
+import kotlin.io.path.moveTo
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.ConfigurableFileCollection
@@ -22,8 +27,9 @@ import org.gradle.api.provider.ValueSource
 import org.gradle.api.provider.ValueSourceParameters
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.jvm.toolchain.JavaLanguageVersion
@@ -34,9 +40,21 @@ import org.gradle.process.ExecOperations
 public abstract class CreateR8LibraryTask : DefaultTask() {
   @get:Inject public abstract val execOperations: ExecOperations
   @get:Classpath public abstract val r8compilerClasspath: ConfigurableFileCollection
-  @get:InputFile public abstract val inputJar: RegularFileProperty
-  @get:InputFile public abstract val pgConfig: RegularFileProperty
-  @get:Input public abstract val enableKeepAnnotations: Property<Boolean>
+  @get:Classpath public abstract val inputJar: RegularFileProperty
+  @get:[Classpath Optional]
+  public abstract val inputClasspath: ConfigurableFileCollection
+  @get:[Classpath Optional]
+  public abstract val replaceInOutputJar: RegularFileProperty
+  @get:InputFiles public abstract val pgConfigs: ConfigurableFileCollection
+  @get:Input
+  public val enableKeepAnnotations: Property<Boolean> =
+    project.objects.property(Boolean::class.java).convention(false)
+  @get:Input
+  public val enableHorizontalClassMerging: Property<Boolean> =
+    project.objects.property(Boolean::class.java).convention(false)
+  @get:Input
+  public val excludingDepsVariant: Property<Boolean> =
+    project.objects.property(Boolean::class.java).convention(false)
   @get:Input
   public val gitHeadSha: Provider<String> =
     project.providers.of(GitHeadShaSource::class.java) {
@@ -70,8 +88,12 @@ public abstract class CreateR8LibraryTask : DefaultTask() {
   @TaskAction
   public fun create() {
     val versionLabel = getVersionLabel(r8compilerClasspath.single())
-    val r8Version = if (versionLabel == "main") gitHeadSha.get() else versionLabel
+    val r8Version =
+      if (versionLabel == "main")
+        gitHeadSha.get() + if (excludingDepsVariant.get()) "+excldeps" else ""
+      else versionLabel
 
+    val outputJarFile = outputJar.get().asFile
     execOperations.javaexec {
       classpath = r8compilerClasspath
       mainClass.set("com.android.tools.r8.R8")
@@ -81,6 +103,9 @@ public abstract class CreateR8LibraryTask : DefaultTask() {
       systemProperty("com.android.tools.r8.enableKeepAnnotations", enableKeepAnnotations.get())
       systemProperty("com.android.tools.r8.enableEmptyMemberRulesToDefaultInitRuleConversion", "0")
       systemProperty("com.android.tools.r8.tracereferences.obfuscateAllEnums", "true")
+      if (enableHorizontalClassMerging.get()) {
+        systemProperty("com.android.tools.r8.disableHorizontalClassMerging", 0)
+      }
 
       val myArgs = mutableListOf(inputJar.get().asFile.absolutePath)
       myArgs.apply {
@@ -90,7 +115,7 @@ public abstract class CreateR8LibraryTask : DefaultTask() {
         add("--source-file-template")
         add("R8_%MAP_ID_%MAP_HASH")
         add("--output")
-        add(outputJar.get().asFile.absolutePath)
+        add(outputJarFile.absolutePath)
         add("--pg-conf-output")
         add(outputPgConfig.get().asFile.absolutePath)
         add("--pg-map-output")
@@ -99,19 +124,58 @@ public abstract class CreateR8LibraryTask : DefaultTask() {
         add(outputPartitionMap.get().asFile.absolutePath)
         add("--lib")
         add(javaHome.get().asFile.absolutePath)
-        add("--pg-conf")
-        add(pgConfig.get().asFile.absolutePath)
+        pgConfigs.forEach { config ->
+          add("--pg-conf")
+          add(config.absolutePath)
+        }
+        inputClasspath.forEach {
+          add("--classpath")
+          add(it.absolutePath)
+        }
       }
       args = myArgs
     }
     if (!enableKeepAnnotations.get()) { // delete api database if we do not enable keep annotations
       // The URI must be prefixed with "jar:" to tell the FileSystem provider to treat it as a
       // ZIP/JAR
-      val uri = create("jar:${outputJar.get().asFile.toPath().toUri()}")
+      val uri = create("jar:${outputJarFile.toPath().toUri()}")
       val env = mapOf("create" to "false")
       newFileSystem(uri, env).use { zipfs ->
         val pathInZip = zipfs.getPath("resources/api_database.ser")
         deleteIfExists(pathInZip)
+      }
+    }
+    if (replaceInOutputJar.isPresent) {
+      val tempOutFile = File.createTempFile("replaced_", ".jar")
+      mergeReplacingContents(outputJarFile, replaceInOutputJar.get().asFile, tempOutFile)
+      tempOutFile.toPath().moveTo(outputJarFile.toPath(), true)
+    }
+  }
+}
+
+private fun mergeReplacingContents(inputJar: File, replaceJar: File, outputJar: File) {
+  ZipFile(inputJar).use { input ->
+    ZipFile(replaceJar).use { replace ->
+      ZipOutputStream(FileOutputStream(outputJar)).use { outStream ->
+        val skipFromInput = mutableSetOf<String>()
+        for (entry in replace.entries()) {
+          val name = entry.name
+          if (name.endsWith(".class")) {
+            outStream.putNextEntry(ZipEntry(entry).apply { time = 0L })
+            replace.getInputStream(entry).use { input -> input.copyTo(outStream) }
+            outStream.closeEntry()
+            skipFromInput.add(name)
+          } else {
+            check(name == "META-INF/MANIFEST.MF" || entry.isDirectory)
+          }
+        }
+        for (entry in input.entries()) {
+          if (entry.name !in skipFromInput) {
+            outStream.putNextEntry(ZipEntry(entry).apply { time = 0L })
+            input.getInputStream(entry).use { input -> input.copyTo(outStream) }
+            outStream.closeEntry()
+          }
+        }
       }
     }
   }
