3db149a8b338f7c59994980a5da7030d1175f64b
Christoffer Adamsen <christofferqa@google.com>
2026-08-28 16:06:54 +0200

    Reapply "Rename EnumUnboxer, NumberUnboxer and Outliner"
    This reverts commit 5e19e63e1c9d64ba0c747c1e4b2658db25d3fddd.
    
    Change-Id: Ibae7be06b220230791e6114d36801c85af39a363

--- src/main/java/com/android/tools/r8/ir/optimize/enums/EnumUnboxingCandidateAnalysis.java
+++ src/main/java/com/android/tools/r8/ir/optimize/enums/EnumUnboxingCandidateAnalysis.java
@@ -30,12 +30,12 @@ class EnumUnboxingCandidateAnalysis {
   private static final int MAX_INSTANCE_FIELDS_FOR_UNBOXING = 7;
 
   private final AppView<AppInfoWithLiveness> appView;
-  private final EnumUnboxerImpl enumUnboxer;
+  private final EnumUnboxer enumUnboxer;
   private final DexItemFactory factory;
   private final EnumUnboxingCandidateInfoCollection enumToUnboxCandidates =
       new EnumUnboxingCandidateInfoCollection();
 
-  EnumUnboxingCandidateAnalysis(AppView<AppInfoWithLiveness> appView, EnumUnboxerImpl enumUnboxer) {
+  EnumUnboxingCandidateAnalysis(AppView<AppInfoWithLiveness> appView, EnumUnboxer enumUnboxer) {
     this.appView = appView;
     this.enumUnboxer = enumUnboxer;
     factory = appView.dexItemFactory();
