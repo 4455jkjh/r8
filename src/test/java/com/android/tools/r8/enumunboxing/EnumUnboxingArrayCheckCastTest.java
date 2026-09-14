3e839cab9306163a20c967cb983babacc7a804f8
Christoffer Adamsen <christofferqa@google.com>
2026-09-14 11:35:33 +0200

    Add EnumUnboxingArrayCheckCastTest
    Bug: b/557269850
    Change-Id: I1e3c9b151e9b9b99881a8b4e78c18e00b7d6234d

---
+++ src/test/java/com/android/tools/r8/enumunboxing/EnumUnboxingArrayCheckCastTest.java
@@ -0,0 +1,83 @@
+// Copyright (c) 2026, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.enumunboxing;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class EnumUnboxingArrayCheckCastTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testRuntime() throws Exception {
+    testForRuntime(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("ClassCastException");
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableInliningAnnotations()
+        .enableNeverClassInliningAnnotations()
+        .addEnumUnboxingInspector(inspector -> inspector.assertUnboxed(Role.class))
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), Main.class)
+        // Due to unsound type narrowing in analyzeCheckCast, (Role[]) o is rewritten to (int[]) o,
+        // allowing int[] to pass the cast and grant admin instead of throwing ClassCastException.
+        // TODO(b/557269850): Should preserve CCE.
+        .assertSuccessWithOutputLines("ADMIN GRANTED");
+  }
+
+  @NeverClassInline
+  public enum Role {
+    USER,
+    ADMIN
+  }
+
+  public static class Main {
+    @NeverInline
+    public static void checkRoles(Object obj) {
+      try {
+        Role[] roles = (Role[]) obj;
+        for (Role r : roles) {
+          if (r == Role.ADMIN) {
+            System.out.println("ADMIN GRANTED");
+          }
+        }
+      } catch (ClassCastException e) {
+        System.out.println("ClassCastException");
+      }
+    }
+
+    public static void main(String[] args) {
+      // Keep enum values live so Role is eligible for unboxing.
+      if (System.currentTimeMillis() < 0) {
+        System.out.println(Role.USER.name());
+        System.out.println(Role.ADMIN.name());
+      }
+      // In unboxed representation, Role.ADMIN is ordinal 1 + 1 = 2.
+      checkRoles(new int[] {2});
+    }
+  }
+}
