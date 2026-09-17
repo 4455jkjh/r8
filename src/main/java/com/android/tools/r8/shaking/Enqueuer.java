4b071f9e6fa154eb8ba07b1ecac3b08399143073
Christoffer Adamsen <christofferqa@google.com>
2026-09-17 12:22:54 +0200

    Commit pending dex items after tree shaking
    Fixes: b/561844403
    Change-Id: I31fbec5d8e8279c8a5360185bd1769aee9b6ba02

--- src/main/java/com/android/tools/r8/shaking/Enqueuer.java
+++ src/main/java/com/android/tools/r8/shaking/Enqueuer.java
@@ -3972,6 +3972,9 @@ public class Enqueuer {
     taskCollection.awaitEnqueuerIndependentTasks();
     analyses.done(this, executorService);
     timing.end();
+    timing.begin("Commit pending dex items");
+    appView.dexItemFactory().commitPendingItems();
+    timing.end();
     assert verifyKeptGraph();
     timing.begin("Finish compat building");
     if (mode.isInitialTreeShaking() && forceProguardCompatibility) {
@@ -4942,14 +4945,6 @@ public class Enqueuer {
       while (true) {
         timing.begin("Compute fixpoint #" + round++);
         long numberOfLiveItems = getNumberOfLiveItems();
-
-        // During the initial round of tree shaking we concurrently parse the code objects. Commit
-        // dex items before every wave to minimize synchronization overhead.
-        if (mode.isInitialTreeShaking()) {
-          timing.begin("Commit pending dex items");
-          withAppReadLock(appView.dexItemFactory()::commitPendingItems);
-          timing.end();
-        }
 
         timing.begin("Process worklist");
         worklist.process(timing);
