5745120c3ffd3a3c986eea1e33830f9a147b9f53
Jonathan Starup <jonathanlist@google.com>
2026-09-22 11:31:04 +0000

    Reuse --force-<assertion option> in CLI parsers.
    This was the last code in BaseCompilerCommandParser, so I just moved it
    to CliParserUtils and removed the empty inheritance from the parsers.
    
    Change-Id: I20b8a4998fcf99c8551508d1ab556cc912e35359

--- src/main/java/com/android/tools/r8/L8CommandParser.java
+++ src/main/java/com/android/tools/r8/L8CommandParser.java
@@ -15,7 +15,7 @@ import com.android.tools.r8.utils.internal.StringUtils;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class L8CommandParser extends BaseCompilerCommandParser {
+public class L8CommandParser {
 
   static String getUsageMessage() {
     return CliParserUtils.getUsageMessage(createParser());
@@ -149,36 +149,8 @@ public class L8CommandParser extends BaseCompilerCommandParser {
             (state, arg) ->
                 state.builder.addDesugaredLibraryConfiguration(
                     StringResource.fromFile(Paths.get(arg))))
-        .prefix0(
-            "--force-enable-assertions",
-            "[:[<class name>|<package name>...]]",
-            "Forcefully enable javac generated assertion code.",
-            (state, suffix) -> parseForceEnableAssertions(state.builder, suffix, state.origin),
-            "--force-ea")
-        .prefix0(
-            "--force-disable-assertions",
-            "[:[<class name>|<package name>...]]",
-            "Forcefully disable javac generated assertion code. This is the default handling of"
-                + " javac assertion code when generating DEX file format.",
-            (state, suffix) -> parseForceDisableAssertions(state.builder, suffix, state.origin),
-            "--force-da")
-        .prefix0(
-            "--force-passthrough-assertions",
-            "[:[<class name>|<package name>...]]",
-            "Don't change javac generated assertion code. This is the default handling of"
-                + " javac assertion code when generating class file format.",
-            (state, suffix) -> parseForcePassthroughAssertions(state.builder, suffix, state.origin),
-            "--force-pa")
-        .prefix0(
-            "--force-assertions-handler",
-            ":<handler method>[:[<class name>|<package name>...]]",
-            "Change javac and kotlinc generated assertion code to invoke the method <handler"
-                + " method> with each assertion error instead of throwing it. The <handler"
-                + " method> is specified as a class name followed by a dot and the method name."
-                + " The handler method must take a single argument of type java.lang.Throwable"
-                + " and have return type void.",
-            (state, suffix) -> parseForceAssertionsHandler(state.builder, suffix, state.origin),
-            "--force-ah")
+        .apply(
+            CliParserUtils.addForceAssertionOptions(state -> state.builder, state -> state.origin))
         .option1(
             "--thread-count",
             "<number>",
--- src/main/java/com/android/tools/r8/utils/CliParserUtils.java
+++ src/main/java/com/android/tools/r8/utils/CliParserUtils.java
@@ -3,22 +3,29 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.utils;
 
+import com.android.tools.r8.AssertionsConfiguration;
+import com.android.tools.r8.BaseCompilerCommand;
 import com.android.tools.r8.Diagnostic;
 import com.android.tools.r8.DiagnosticsLevel;
 import com.android.tools.r8.ParseFlagInfo;
 import com.android.tools.r8.ParseFlagInfoImpl;
 import com.android.tools.r8.ParseFlagPrinter;
 import com.android.tools.r8.origin.Origin;
+import com.android.tools.r8.references.MethodReference;
+import com.android.tools.r8.references.Reference;
 import com.android.tools.r8.utils.internal.CliParser;
 import com.android.tools.r8.utils.internal.CliParserBase;
 import com.android.tools.r8.utils.internal.CliParserBase.HelpInfo;
 import com.android.tools.r8.utils.internal.CliParserBase.OptionInfo;
 import com.android.tools.r8.utils.internal.StringUtils;
+import com.android.tools.r8.utils.internal.collections.Pair;
+import com.android.tools.r8.utils.internal.exceptions.Unreachable;
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.IntConsumer;
 import java.util.function.Predicate;
 
@@ -253,5 +260,179 @@ public class CliParserUtils {
                     error -> errorHandler.accept(state, "Invalid argument to --min-api: " + error));
               }
             });
+  }
+
+  public static <T, C extends BaseCompilerCommand, B extends BaseCompilerCommand.Builder<C, B>>
+      Consumer<CliParser<T>> addForceAssertionOptions(
+          Function<T, B> getBuilder, Function<T, Origin> getOrigin) {
+    return parser ->
+        parser
+            .prefix0(
+                "--force-enable-assertions",
+                "[:[<class name>|<package name>...]]",
+                "Forcefully enable javac generated assertion code.",
+                (state, suffix) -> {
+                  B builder = getBuilder.apply(state);
+                  String scope = parseAssertionScope(builder, suffix, getOrigin.apply(state));
+                  addAssertionTransformation(
+                      builder, AssertionTransformationType.ENABLE, null, scope);
+                },
+                "--force-ea")
+            .prefix0(
+                "--force-disable-assertions",
+                "[:[<class name>|<package name>...]]",
+                "Forcefully disable javac generated assertion code. This is the default handling of"
+                    + " javac assertion code when generating DEX file format.",
+                (state, suffix) -> {
+                  B builder = getBuilder.apply(state);
+                  String scope = parseAssertionScope(builder, suffix, getOrigin.apply(state));
+                  addAssertionTransformation(
+                      builder, AssertionTransformationType.DISABLE, null, scope);
+                },
+                "--force-da")
+            .prefix0(
+                "--force-passthrough-assertions",
+                "[:[<class name>|<package name>...]]",
+                "Don't change javac generated assertion code. This is the default handling of"
+                    + " javac assertion code when generating class file format.",
+                (state, suffix) -> {
+                  B builder = getBuilder.apply(state);
+                  String scope = parseAssertionScope(builder, suffix, getOrigin.apply(state));
+                  addAssertionTransformation(
+                      builder, AssertionTransformationType.PASSTHROUGH, null, scope);
+                },
+                "--force-pa")
+            .prefix0(
+                "--force-assertions-handler",
+                ":<handler method>[:[<class name>|<package name>...]]",
+                "Change javac and kotlinc generated assertion code to invoke the method <handler"
+                    + " method> with each assertion error instead of throwing it. The <handler"
+                    + " method> is specified as a class name followed by a dot and the method name."
+                    + " The handler method must take a single argument of type java.lang.Throwable"
+                    + " and have return type void.",
+                (state, suffix) -> {
+                  B builder = getBuilder.apply(state);
+                  Pair<MethodReference, String> handlerAndScope =
+                      parseAssertionHandler(builder, suffix, getOrigin.apply(state));
+                  addAssertionTransformation(
+                      builder,
+                      AssertionTransformationType.HANDLER,
+                      handlerAndScope.getFirst(),
+                      handlerAndScope.getSecond());
+                },
+                "--force-ah");
+  }
+
+  private enum AssertionTransformationType {
+    ENABLE,
+    DISABLE,
+    PASSTHROUGH,
+    HANDLER
+  }
+
+  private static AssertionsConfiguration.Builder prepareBuilderForScope(
+      AssertionsConfiguration.Builder builder,
+      AssertionTransformationType transformation,
+      MethodReference assertionHandler) {
+    switch (transformation) {
+      case ENABLE:
+        return builder.setCompileTimeEnable();
+      case DISABLE:
+        return builder.setCompileTimeDisable();
+      case PASSTHROUGH:
+        return builder.setPassthrough();
+      case HANDLER:
+        return builder.setAssertionHandler(assertionHandler);
+      default:
+        throw new Unreachable();
+    }
+  }
+
+  private static <C extends BaseCompilerCommand, B extends BaseCompilerCommand.Builder<C, B>>
+      void addAssertionTransformation(
+          B builder,
+          AssertionTransformationType transformation,
+          MethodReference assertionHandler,
+          String scope) {
+    if (scope == null) {
+      builder.addAssertionsConfiguration(
+          b -> prepareBuilderForScope(b, transformation, assertionHandler).setScopeAll().build());
+    } else {
+      assert !scope.isEmpty();
+      String packageAssertionPostfix = "...";
+      if (scope.endsWith(packageAssertionPostfix)) {
+        builder.addAssertionsConfiguration(
+            b ->
+                prepareBuilderForScope(b, transformation, assertionHandler)
+                    .setScopePackage(
+                        scope.substring(0, scope.length() - packageAssertionPostfix.length()))
+                    .build());
+      } else {
+        builder.addAssertionsConfiguration(
+            b ->
+                prepareBuilderForScope(b, transformation, assertionHandler)
+                    .setScopeClass(scope)
+                    .build());
+      }
+    }
+  }
+
+  private static <C extends BaseCompilerCommand, B extends BaseCompilerCommand.Builder<C, B>>
+      String parseAssertionScope(B builder, String suffix, Origin origin) {
+    if (suffix.isEmpty()) {
+      return null;
+    }
+    if (suffix.equals(":")) {
+      throw builder.fatalError(new StringDiagnostic("Missing optional argument", origin));
+    }
+    if (!suffix.startsWith(":")) {
+      builder.error(new StringDiagnostic("Illegal assertion scope: " + suffix, origin));
+      return null;
+    }
+    String classOrPackageScope = suffix.substring(1);
+    if (classOrPackageScope.contains(";")
+        || classOrPackageScope.contains("[")
+        || classOrPackageScope.contains("/")) {
+      builder.error(
+          new StringDiagnostic("Illegal assertion scope: " + classOrPackageScope, origin));
+    }
+    return classOrPackageScope;
+  }
+
+  private static <C extends BaseCompilerCommand, B extends BaseCompilerCommand.Builder<C, B>>
+      Pair<MethodReference, String> parseAssertionHandler(B builder, String suffix, Origin origin) {
+    if (suffix.isEmpty() || suffix.equals(":")) {
+      throw builder.fatalError(
+          new StringDiagnostic("Missing required argument <handler method>", origin));
+    }
+    if (!suffix.startsWith(":")) {
+      throw builder.fatalError(
+          new StringDiagnostic("Missing required argument <handler method>", origin));
+    }
+    String remaining = suffix.substring(1);
+    int index = remaining.indexOf(':');
+    if (index == 0) {
+      throw builder.fatalError(
+          new StringDiagnostic("Missing required argument <handler method>", origin));
+    }
+    String assertionsHandlerString = index > 0 ? remaining.substring(0, index) : remaining;
+    int lastDotIndex = assertionsHandlerString.lastIndexOf('.');
+    if (assertionsHandlerString.length() < 3
+        || lastDotIndex <= 0
+        || lastDotIndex == assertionsHandlerString.length() - 1
+        || !DescriptorUtils.isValidJavaType(assertionsHandlerString.substring(0, lastDotIndex))) {
+      throw builder.fatalError(
+          new StringDiagnostic(
+              "Invalid argument <handler method>: " + assertionsHandlerString, origin));
+    }
+    MethodReference assertionsHandler =
+        Reference.methodFromDescriptor(
+            DescriptorUtils.javaTypeToDescriptor(
+                assertionsHandlerString.substring(0, lastDotIndex)),
+            assertionsHandlerString.substring(lastDotIndex + 1),
+            "(Ljava/lang/Throwable;)V");
+    String scopeSuffix = remaining.substring(assertionsHandlerString.length());
+    String scope = parseAssertionScope(builder, scopeSuffix, origin);
+    return Pair.create(assertionsHandler, scope);
   }
 }
